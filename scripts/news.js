9f82de190ec1b8e3f2bef721b24135b633019c62
jhermk <lakoffmanie2018@gmail.com>
2023-04-12 04:55:35 +0000

    update

--- scripts/news.js
+++ scripts/news.js
@@ -1,25 +1,30 @@
   // news api
-  const KEY = 'cb7488eff8ed4a3aafa4380e32f97eb4';
+  const KEY = 'lCxcgARC80c4FgP3vVqbkFwLqMaQjLnQv-x3rFbQxqY';
   const COASTAL_AREA = 'carlsbad beach';
   
-  fetch(`https://newsapi.org/v2/everything?q=${COASTAL_AREA}&apiKey=${KEY}`)
-    .then(response => {
-      if (!response.ok) {
-        throw new Error('Network response was not ok');
-      }
-      return response.json();
-    })
-    .then(data => {
-      const articles = data.articles;
-      const articleList = document.querySelectorAll('.article');
-      for (let i = 0; i < articleList.length; i++) {
-        const article = articles[i];
-        articleList[i].querySelector('img').src = article.urlToImage;
-        articleList[i].querySelector('h3').innerText = article.title;
-        articleList[i].querySelector('p').innerText = article.description;
-        articleList[i].querySelector('.read-more').href = article.url;
-      }
-    })
-    .catch(error => {
-      console.error('There was a problem with the fetch request:', error);
-    });
+  fetch(`https://api.newscatcherapi.com/v2/search?q=${COASTAL_AREA}&lang=en&sort_by=relevancy&page=1`, {
+    headers: {
+      'x-api-key': KEY
+    }
+  })
+  .then(response => {
+    if (!response.ok) {
+      throw new Error('Network response was not ok');
+    }
+    return response.json();
+  })
+  .then(data => {
+    const articles = data.articles;
+    const articleList = document.querySelectorAll('.article');
+    for (let i = 0; i < articleList.length; i++) {
+      const article = articles[i];
+      articleList[i].querySelector('img').src = article.media;
+      articleList[i].querySelector('h3').innerText = article.title;
+      articleList[i].querySelector('p').innerText = article.summary;
+      articleList[i].querySelector('.read-more').href = article.link;
+    }
+  })
+  .catch(error => {
+    console.error('There was a problem with the fetch request:', error);
+  });
+  
